5d37bf6f4041240f76b97d8d58d4f23b7d5ca90e
Leandro Fratel de Souza <leandrofratel@gmail.com>
2020-04-13 22:14:12 -0300

    Aula de laço

---
+++ mundo-02/Aula-01.py
@@ -0,0 +1,43 @@
+# Contagem regressiva para os fogos.
+'''
+from time import sleep
+for c in range(10, 0, -1):
+    sleep(1)
+    print(c)
+print('\033[1;31mBOOOOMMMMM!!!\033[m')
+'''
+# Imprima os numeros pares de 1 a 50.
+'''
+for c in range(0, 52, 2):
+    print(c)
+'''
+# Soma dos numeros impares multiplos de 3 no intervalo de 1 a 500.
+'''
+'''
+# Faça uma tabuada igual ao exercio 009 utilizando laço.
+'''
+'''
+# Faça um programa que leia e some 6 numeros pares, se for impar desconsiderar.
+'''
+'''
+# Progressão aritmetica?
+'''
+'''
+# Um programa que leia um número inteiro e diga se é ou não um primo.
+'''
+'''
+# Faça um progrma que leia uma frase e diga se ela é um palindromoo.
+'''
+'''
+# Um programa que leia o ano de nascimento e diga quantos são maoir de idade e quantos não (21 anos).
+'''
+'''
+# Um programa que leia o peso de 5 pessoas e diga qual é o maior e qual é o menor.
+'''
+'''
+# Um programa que leia o Nome, Idade e o Sexo de 4 pessoas e diga:
+# A média de idade do grupo;
+# Qual o nome do homem mais velho;
+# Quantas mulheres tem menos de 20 anos.
+'''
+'''
